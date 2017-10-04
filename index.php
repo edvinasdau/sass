bc00b4ee22169165dd02ec83fe31aa232747aae2
edvinasdau <30698620+edvinasdau@users.noreply.github.com>
2017-10-04 14:58:57 +0300

    a

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tinklapis</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+	<link rel="stylesheet" href="../../font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row virsus">
+			<div class="col bloom">Bloom</div>
+			<div class="col f">
+				<button type="button" class="myg btn btn-success">Home</button>
+				<button type="button" class="myg btn">Service</button>
+				<button type="button" class="myg btn">Portfolio</button>
+				<button type="button" class="myg btn">Contact</button>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row a">
+			<div class="col"><p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci. </p></div>
+		</div>	
+		<div class="row b">
+			<div class="col z">
+				<button type="button" class="btn btn-success">Pirmas mygtukas</button>
+				<button type="button" class="btn btn-success">Antras mygtukas</button>
+			</div>
+
+		</div>
+	</div>
+	<div class="container">
+		<div class="row c">
+			<div class="col"><h1>My portfolio</h1></div>
+		</div>
+
+		<div class="row d">
+			<div class="col-md-3"><img src="images/img.jfif"></div>
+
+			<div class="col e"><p>1Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p></div>
+
+			<div class="col-md-3"><img src="images/img.jfif"></div>
+
+			<div class="col e"><p>2Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p></div>
+		</div>	
+
+		<div class="row d">
+			<div class="col-md-3"><img src="images/img.jfif"></div>
+			<div class="col e"><p>3Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p></div>
+			<div class="col-md-3"><img src="images/img.jfif"></div>
+			<div class="col e"><p>4Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</div>
+			</div>
+
+			<div class="row">
+				<div class="col g"><p>Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere. Praesent turpis.</p></div>
+			</div>
+
+			<div class="row i">
+				<div class="col i">
+					<div class="col-8 h"><p>Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, </p><button type="button" class="btn btn-success">Home</button></div>
+					<div class="col-4 h"><p>Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam</p></div>
+				</div>	
+			</div>	
+			<div class="row">
+				<div class="col footer"><p>Copyright (c) 2013. All rights reserved.</p></div>
+			</div>	
+		</div>
+	</body>
+	</html>
